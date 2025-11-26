e7a7aaaa866df3cbc284865b1533c6a9b544df58
Craig <craig.hickman@ukaea.uk>
2025-11-26 21:53:49 +0000

    Almost working! Just one dynamic type system error

---
+++ lib/main_8.dart
@@ -0,0 +1,244 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+// ------------------- Data Layer (Persistence, Serialization, Transfer, etc) ------------------
+// This is a lower level representation of the data used in the application. It is structured for efficient storage and transfer, and is not directly
+// used by business logic or UI layers. It may be organized into tables with relationships rather than structures that represent the domain model.
+// There is sometimes a Repository layer that abstracts access to the data layer, but for simplicity we are skipping that here.
+
+class DatabaseAccess {
+  Future<int> loadCounter() async {
+    // Simulate loading from a database
+    await Future.delayed(const Duration(milliseconds: 100));
+    return 0;
+  }
+
+  Future<void> saveCounter(int counter) async {
+    // Simulate saving to a database
+    await Future.delayed(const Duration(milliseconds: 100));
+  }
+}
+
+// ------------------- Domain Layer ------------------
+// The data used to define the state of the application (defines how the UI looks at any point in time)
+// It is a modeled after a domain area (e.g robotics, banking, fitness, etc) and can use structures that best represent that domain.
+// In this example, we have a simple counter application, so the domain model is just a single integer value.
+
+class CounterModel {
+  final int counter;
+  const CounterModel(this.counter);
+}
+
+// ------------------- Business Logic Layer ------------------
+// It is tempting to define methods on the CounterModel itself to mutate its state (e.g increment, decrement), or to define extension methods on it,
+// but by using controller objects that contain a reference to the model, we can restrict UI components from being able to mutate the model unless they have access to a controller.
+// This helps enforce separation of concerns.
+// It is tempting to put a ValueNotifier inside the controller, but that would couple the controller to a specific state management approach.
+// Instead, we pass in a callback that the controller can use to notify when the model has changed, and we wire it all together in the application entry point.
+// We can separate controllers for different use cases (e.g incrementing vs decrementing), so that UI components only have access to the functionality they need.
+
+class CounterUpController {
+  CounterModel _model;
+  final void Function(CounterModel newModel) onModelChanged;
+  CounterUpController(this._model, this.onModelChanged);
+  void incrementCounter() {
+    _model = CounterModel(_model.counter + 1);
+    onModelChanged(_model);
+  }
+}
+
+class CounterDownController {
+  CounterModel _model;
+  final void Function(CounterModel newModel) onModelChanged;
+  CounterDownController(this._model, this.onModelChanged);
+  void decrementCounter() {
+    _model = CounterModel(_model.counter - 1);
+    onModelChanged(_model);
+  }
+}
+
+// ------------------- Glue Layer ------------------
+
+typedef ViewBuilder<Model, Controller> = Widget Function(Model model, Controller controller);
+typedef ControllerBuilder<Model> = dynamic Function(Model model, void Function(Model newModel) onModelChanged);
+class ViewAndControllerPair<Model, Controller> {
+  final ViewBuilder<Model, Controller> viewBuilder;
+  final ControllerBuilder<Model> controllerBuilder;
+  ViewAndControllerPair({required this.viewBuilder, required this.controllerBuilder});
+}
+
+// A generalized configuration class to build the app
+class RoutingAndControllerConfig<Model> {
+  ValueNotifier<Model> modelNotifier;//Model can be any type
+  Map<String, ViewAndControllerPair<Model, dynamic>> routesToViewsAndControllers;
+  RoutingAndControllerConfig(this.modelNotifier, this.routesToViewsAndControllers);
+
+  Widget buildApp() {
+    debugPrint('Building app with modelNotifier: $modelNotifier');
+    return ValueListenableBuilder(
+      valueListenable: modelNotifier,
+      builder: (_, __, ___) {
+        debugPrint('UI updated triggered with model value: ${modelNotifier.value}');
+        return MaterialApp(
+          initialRoute: '/',
+          routes: buildRoutes()
+        );
+      }
+    );
+  }
+
+  //programmatically return a map of strings to widget builders based on the config. This is where we map:
+  //Which routes go to which views
+  //Which Views get which controllers
+  //What controllers do onModelChanged (For now, they will all update the same modelNotifier)
+  //convert ViewBuilder: F(M,C)=>W into WidgetBuilder: F(BuildContext)=>W
+  //widgetBuilderN will return the correct view, and that view will have the correct controller wired in
+  Map<String, WidgetBuilder> buildRoutes() {
+    Map<String, ViewAndControllerPair<Model, dynamic>> routesToViewsAndControllers = this.routesToViewsAndControllers;
+    Map<String, WidgetBuilder> routes = {};
+    debugPrint('Building routes map with keys: ${routesToViewsAndControllers.keys}');
+    routesToViewsAndControllers.forEach((routeN, viewAndControllerBuilderN) {
+      debugPrint('Wiring route $routeN');
+      final controllerBuilderN = viewAndControllerBuilderN.controllerBuilder; //"Error retrieving viewBuilder for route /: type '(CounterModel, CounterUpController) => CounterUpView' is not a subtype of type '(CounterModel, dynamic) => Widget'"
+      debugPrint('!');
+      final ViewBuilder<Model, dynamic> viewBuilderN = viewAndControllerBuilderN.viewBuilder;
+      debugPrint('Building widget builder for route $routeN');
+      final widgetBuilderN = (_) {
+        final model = modelNotifier.value;
+        final onChangedCallback = (newValue) => modelNotifier.value = newValue;
+        debugPrint('Building controller for route $routeN with model value: $model and onChangedCallback: $onChangedCallback');
+        final controllerN = controllerBuilderN(model, onChangedCallback);
+        debugPrint('Building view for route $routeN with model value: $model and controller: $controllerN');
+        final viewN = viewBuilderN(model, controllerN);
+        debugPrint('Built view for route $routeN: $viewN');
+        return viewN;
+      };
+      debugPrint('Assigning $widgetBuilderN to route $routeN');
+      routes[routeN] = widgetBuilderN;
+      debugPrint('Assigned $widgetBuilderN to route $routeN');
+    });
+    return routes;
+  }
+
+}
+
+void main() {
+  final db = DatabaseAccess();
+  final modelNotifier = ValueNotifier(CounterModel(0));
+  modelNotifier.addListener(() => db.saveCounter(modelNotifier.value.counter));
+
+  // runApp(
+  //   ValueListenableBuilder(
+  //     valueListenable: modelNotifier,
+  //     builder: (_, __, ___) => MaterialApp(
+  //       initialRoute: '/',
+  //       routes: {
+  //         '/' : (_) => CounterUpView(model: modelNotifier.value, controller: CounterUpController(modelNotifier.value, (newValue) => modelNotifier.value = newValue)),
+  //         '/down': (_) => CounterDownView(model: modelNotifier.value, controller: CounterDownController(modelNotifier.value, (newValue) => modelNotifier.value = newValue)),
+  //       },
+  //     ),
+  //   ),
+  // );
+
+  debugPrint('Creating RoutingAndControllerConfig');
+  final config = RoutingAndControllerConfig(
+    modelNotifier,
+    {
+      '/' : ViewAndControllerPair<CounterModel, CounterUpController>(
+        viewBuilder: (m, c) => CounterUpView(model: m, controller: c),
+        controllerBuilder: (m, c) => CounterUpController(m, c),
+      ),
+      '/down' : ViewAndControllerPair<CounterModel, CounterDownController>(
+        viewBuilder: (m, c) => CounterDownView(model: m, controller: c),
+        controllerBuilder: (m, c) => CounterDownController(m, c),
+      ),
+    }
+  );
+  debugPrint('Running app from config');
+  runApp(config.buildApp());
+}
+
+// ------------------- UI Layer: Views ------------------
+// Pages are the main screens of our application. They are responsible for declaratively composing smaller widgets together to form the full screen UI.
+// Pages have access to the domain model and appropriate controllers to handle user interactions.
+// They pass the appropriate data from the domain model into it's display widgets, and it passes appropriate controller methods into the interaction widgets.
+// It should not pass the entire model or controller down to child widgets, only the data and methods that those child widgets need. Pages are the only Widgets
+// that have access to the model or controller objects. This keeps the widgets "dumb". They simply display the data they are given, and call the methods they
+// are given when user interactions occur.
+// Pages should not use reference theme data directly. The child widgets should use the context to retrieve theme data as needed. 
+
+class CounterUpView extends StatelessWidget {
+  final CounterModel model;
+  final CounterUpController controller;
+  const CounterUpView({super.key, required this.model, required this.controller});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: CenteredValue(value: model.counter),
+    floatingActionButton: PlusMinusFloatingActionButton(onPlus: controller.incrementCounter),
+    appBar: BackForthAppBar(onBack: ()=>Navigator.of(context).pushNamed('/down'), onForth: (){}),
+  );
+}
+
+class CounterDownView extends StatelessWidget {
+  final CounterModel model;
+  final CounterDownController controller;
+  const CounterDownView({super.key, required this.model, required this.controller});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: CenteredValue(value: model.counter),
+    floatingActionButton: PlusMinusFloatingActionButton(onMinus: controller.decrementCounter),
+    appBar: BackForthAppBar(onBack: Navigator.of(context).pop, onForth: ()=>Navigator.of(context).pushNamed('/')),
+  );
+}
+
+
+// ------------------- UI Layer: Components ------------------
+// Widgets are the building blocks of our UI. They should be as dumb as possible. They simply display the data they are given, and call the methods they
+// are given when user interactions occur. They should not have any knowledge of the domain model or controllers.
+// They can use the BuildContext to retrieve theme data as needed. It is better to do that here rather than in the parent, so that widgets have consistent
+// theming across the application.
+
+class CenteredValue extends StatelessWidget {
+  final int value;
+  const CenteredValue({super.key, required this.value});
+  @override
+  Widget build(BuildContext context) => Center(child: Text('Value: $value'));
+}
+
+class BackForthAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback onBack;
+  final VoidCallback onForth;
+  const BackForthAppBar({super.key, required this.onBack, required this.onForth});
+
+  @override
+  Widget build(BuildContext context) => AppBar(
+    actions: [
+      IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: onBack,
+      ),
+      IconButton(
+        icon: const Icon(Icons.arrow_forward),
+        onPressed: onForth,
+      ),
+    ],
+  );
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class PlusMinusFloatingActionButton extends StatelessWidget {
+  final VoidCallback? onPlus;
+  final VoidCallback? onMinus;
+  const PlusMinusFloatingActionButton({super.key, this.onPlus, this.onMinus});
+
+  @override
+  Widget build(BuildContext context) => FloatingActionButton(
+    onPressed: onPlus ?? onMinus,
+    child: Icon(onPlus != null ? Icons.add : Icons.remove),
+  );
+}
